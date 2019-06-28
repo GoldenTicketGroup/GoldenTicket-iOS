d0ebc5ad62dd94e9563a60e88a7abaaaa5c935ab
JaeeunAhn <jaeeunno1@naver.com>
2019-06-28 19:01:28 +0900

    파일 꼬인 거 수정.

---
+++ GoldenTicket/GoldenTicket/Extensions/test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  GoldenTicket
+//
+//  Created by 안재은 on 28/06/2019.
+//  Copyright © 2019 황수빈. All rights reserved.
+//
+
+import Foundation
--- GoldenTicket/GoldenTicket/Sources/ViewControllers/Notice/NoticeViewController.swift
+++ GoldenTicket/GoldenTicket/Sources/ViewControllers/Notice/NoticeViewController.swift
@@ -2,7 +2,7 @@
 //  NoticeViewController.swift
 //  GoldenTicket
 //
-//  Created by 황수빈 on 28/06/2019.
+//  Created by 안재은 on 28/06/2019.
 //  Copyright © 2019 황수빈. All rights reserved.
 //
 
