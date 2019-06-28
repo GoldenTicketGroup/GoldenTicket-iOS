e360896318b6ccdc484e48a782ffc56497595103
ttub-nii <ostarangelo@naver.com>
2019-06-28 16:42:30 +0900

    autolayout 1
    tutorial, login, lose, notice view

---
+++ GoldenTicket/Login/NoticeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NoticeViewController.swift
+//  GoldenTicket
+//
+//  Created by 황수빈 on 28/06/2019.
+//  Copyright © 2019 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
