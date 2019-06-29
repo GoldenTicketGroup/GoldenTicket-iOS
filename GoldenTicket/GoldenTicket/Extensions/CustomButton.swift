51a5eb16b3103f291249572aefbe7fb6f4a487de
JaeeunAhn <jaeeunno1@naver.com>
2019-06-29 16:45:04 +0900

    [CREATE] extenstion 파일들 추가

---
+++ GoldenTicket/GoldenTicket/Extensions/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  GoldenTicket
+//
+//  Created by 안재은 on 28/06/2019.
+//  Copyright © 2019 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 18
+    }
+    
+}
---
+++ GoldenTicket/GoldenTicket/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extensions.swift
+//  GoldenTicket
+//
+//  Created by 안재은 on 28/06/2019.
+//  Copyright © 2019 황수빈. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var peach: UIColor {
+        return UIColor(red: 253.0 / 255.0, green: 164.0 / 255.0, blue: 122.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var maize: UIColor {
+        return UIColor(red: 250.0 / 255.0, green: 217.0 / 255.0, blue: 82.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var darkCream: UIColor {
+        return UIColor(red: 251.0 / 255.0, green: 246.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0)
+    }
+
+}
