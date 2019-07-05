ffff2fff71a6c079631222b189ae6196804babd8
JaeeunAhn <jaeeunno1@naver.com>
2019-07-05 19:14:16 +0900

    [UPDATE] fix flow, detail view, search view, no ticket view

---
+++ GoldenTicket/GoldenTicket/Sources/ViewControllers/Main/LotteryCheckCell.swift
@@ -0,0 +1,16 @@
+//
+//  LotteryCheckCell.swift
+//  GoldenTicket
+//
+//  Created by 안재은 on 05/07/2019.
+//  Copyright © 2019 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class LotteryCheckCell : UICollectionViewCell {
+    @IBOutlet weak var lotteryShowTitle: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+}
