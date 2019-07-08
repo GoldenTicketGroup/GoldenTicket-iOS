a46af669226c0c3e5b8ae8e3cd1089155c0a4e07
JaeeunAhn <jaeeunno1@naver.com>
2019-07-08 12:00:29 +0900

    [UPDATE] 충돌 난 코드 부분 해결

--- GoldenTicket/GoldenTicket/APIServices/ShowService.swift
+++ GoldenTicket/GoldenTicket/APIServices/ShowService.swift
@@ -13,31 +13,21 @@ struct ShowService {
     
     static let shared = ShowService()
     
-    func getShow(_ show_idx: Int, _ image_url : String, _ draw_available: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+    func getShow(showid: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
         
-        let URL = APIConstants.ShowURL + "/\(show_idx)"
+        let URL = APIConstants.ShowURL + "/\(showid)"
         
         let header: HTTPHeaders = [
             "Content-Type" : "application/json",
             "query string" : "id"
         ]
         
-<<<<<<< HEAD
-        let body: Parameters = [
-            "showId" : show_idx,
-            "imageURL" : image_url,
-            "drawAvailable" : draw_available
-        ]
-        
-        Alamofire.request(URL, method: .get, parameters: body, encoding: JSONEncoding.default, headers: header)
-=======
         Alamofire.request(URL, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: header)
->>>>>>> 90753ad062afc891a47f359c2aba0cbb2e6d46af
             .responseData { response in
                 
                 switch response.result {
                     
-                    // 통신 성공
+                // 통신 성공
                 case .success:
                     if let value = response.result.value {
                         if let status = response.response?.statusCode {
@@ -69,7 +59,7 @@ struct ShowService {
                     }
                     break
                     
-                    // 통신 실패
+                // 통신 실패
                 case .failure(let err):
                     print(err.localizedDescription)
                     completion(.networkFail)
@@ -78,4 +68,3 @@ struct ShowService {
         }
     }
 }
-
--- GoldenTicket/GoldenTicket/Sources/ViewControllers/Login/LoginVC.swift
+++ GoldenTicket/GoldenTicket/Sources/ViewControllers/Login/LoginVC.swift
@@ -46,24 +46,19 @@ class LoginVC: UIViewController {
                 
             // 매개변수에 어떤 값을 가져올 것인지
             case .success(let token):
-<<<<<<< HEAD
-                UserDefaults.standard.set(token, forKey: "Token")
-                self.simpleAlert(title: "로그인 성공", message: "")
-=======
                 
                 // ResponseString.swift 파일의 DataClass 구조체에서 token 값만 받아오겠다.
-                let token_string = (token as! DataClass).token 
+                let token_string = (token as! DataClass).token
                 UserDefaults.standard.set(token_string, forKey: "token")
                 
-//                let user_name = (name as! DataClass).name
-//                UserDefaults.standard.set(user_name, forKey: "name")
-//
-//                let user_email = (email as! DataClass).email
-//                UserDefaults.standard.set(user_email, forKey: "email")
-//                
-//                let user_phone = (phone as! DataClass).phone
-//                UserDefaults.standard.set(user_phone, forKey: "phone")
->>>>>>> 90753ad062afc891a47f359c2aba0cbb2e6d46af
+                //                let user_name = (name as! DataClass).name
+                //                UserDefaults.standard.set(user_name, forKey: "name")
+                //
+                //                let user_email = (useremail as! DataClass).email
+                //                UserDefaults.standard.set(user_email, forKey: "email")
+                //
+                //                let user_phone = (phone as! DataClass).phone
+                //                UserDefaults.standard.set(user_phone, forKey: "phone")
                 
                 let storyboardMain = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainVC")
                 self.present(storyboardMain, animated: true)
@@ -165,4 +160,3 @@ extension LoginVC: UIGestureRecognizerDelegate {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
-
