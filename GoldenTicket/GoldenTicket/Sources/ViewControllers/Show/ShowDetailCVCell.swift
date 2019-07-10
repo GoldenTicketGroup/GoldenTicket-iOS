07ee231f7fc9058503aaaca11e80ef6f028ff454
JaeeunAhn <jaeeunno1@naver.com>
2019-07-10 16:16:22 +0900

    [CREATE] detail Image collection view 통신 성공

---
+++ GoldenTicket/GoldenTicket/Sources/ViewControllers/Show/ShowDetailCVCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShowDetailCVCell.swift
+//  GoldenTicket
+//
+//  Created by 안재은 on 10/07/2019.
+//  Copyright © 2019 황수빈. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+}
