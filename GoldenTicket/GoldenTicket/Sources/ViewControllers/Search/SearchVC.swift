6403c50c2e7cd35af80d2f6f112a388824992e97
JaeeunAhn <jaeeunno1@naver.com>
2019-07-12 19:44:05 +0900

    [UPDATE] search detail network, contents view fix

--- GoldenTicket/GoldenTicket/Sources/ViewControllers/Search/SearchVC.swift
+++ GoldenTicket/GoldenTicket/Sources/ViewControllers/Search/SearchVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Hero
+import Kingfisher
 
 class SearchVC: UIViewController {
 
@@ -26,13 +27,19 @@ class SearchVC: UIViewController {
     var checkisLiked : Int?  // 좋아요가 체크 되어있나요? 0: 아니요. 1: 예
     var showIdx : Int?
     
+    var showDetail : ShowDetail!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.isHidden = true
         
         searchField.attributedPlaceholder = NSAttributedString(string: "어떤 공연을 찾고 계신가요?", attributes: [NSAttributedString.Key.foregroundColor: UIColor.peach])
-                
+        
+        // delegate, datasource
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         //텍스트 필드 customize.
 
         searchView.dropShadow(color: UIColor.black16, offSet: CGSize(width: 0, height: 0), opacity: 1, radius: 2)
@@ -172,7 +179,17 @@ extension SearchVC : UICollectionViewDataSource
     }
 }
 
-
+extension SearchVC : UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let show = searchShowList[indexPath.row]
+        self.showIdx = show.show_idx  // 1 ~ 17
+        
+        // data setting
+        self.setDetailData()
+    }
+}
 
 extension SearchVC : UICollectionViewDelegateFlowLayout
 {
@@ -183,5 +200,86 @@ extension SearchVC : UICollectionViewDelegateFlowLayout
         let height: CGFloat = 236
 
         return CGSize(width: width, height: height)
+    }
+}
+
+
+extension SearchVC {
+    
+    func setDetailData() {
+        
+        guard let idx = self.showIdx else { return }
+        
+        // print("idx : \(idx)")
+        ShowService.shared.showDetail(showIdx: idx) {
+            [weak self]
+            data in
+            
+            guard let `self` = self else { return }
+            switch data {
+                
+            case .success(let res):
+                
+                // 1. 공연 하나에 대한 정보만 받아오면 된다.
+                self.showDetail = res as! ShowDetail
+                
+                let dvc = self.storyboard?.instantiateViewController(withIdentifier: "ShowDetailVC") as! ShowDetailVC
+                
+                dvc.showIdx = idx   // 다음 스토리 보드로 index 넘기기
+                
+                // 2. ShowDetail Struct
+                let imageUrlString = self.showDetail.image_url
+                let imageUrl = URL(string: imageUrlString)!
+                let imageData = try! Data(contentsOf: imageUrl)
+                let image = UIImage(data: imageData)
+                
+                // 1. posterImg가 있을 때 imageFromUrl을 호출하는데, 초기화 안된 상태이고 nil이니까 초기화를 해줘야 한다.
+                // 2. UIimageView가 아닌 UIImage로 접근해야 한다.
+                dvc.posterImg = image
+                dvc.showName = self.showDetail.name
+                dvc.showLocation = self.showDetail.location
+                dvc.showTime = self.showDetail.duration
+                dvc.showBeforePrice = self.showDetail.original_price
+                dvc.showAfterPrice = self.showDetail.discount_price
+                dvc.checkisLiked = self.showDetail.is_liked     // 다음 스토리보드로 좋아요 여부 보내기
+                
+                // image URL 얻어오기
+                let imageUrlString2 = self.showDetail.background_image
+                let imageUrl2 = URL(string: imageUrlString2)!
+                let imageData2 = try! Data(contentsOf: imageUrl2)
+                let backimage = UIImage(data: imageData2)
+                
+                dvc.backgroundImg = backimage
+                
+                // 다음 시간표 리스트로 스케줄 서버 통신 받아온 데이터 넘기기
+                dvc.timeList = self.showDetail.schedule!
+                self.present(dvc, animated: true)
+                self.navigationController?.pushViewController(dvc, animated: true)
+                
+                
+                // 2. Poster Struct
+                let poster = self.showDetail.poster
+                dvc.posterList = poster!
+                
+                // 2. Actor Struct
+                let artistDetail = self.showDetail.artist
+                // let artistDetail = res as! [Artist]
+                
+                // dvc에 있는 배우들 리스트에 서버 통신해서 꽂아주기
+                dvc.artistList = artistDetail!
+                
+            case .requestErr(let message):
+                self.simpleAlert(title: "공연 상세 조회 실패", message: "\(message)")
+                
+            case .pathErr:
+                print(".pathErr")
+                
+            case .serverErr:
+                print(".serverErr")
+                
+            case .networkFail:
+                self.simpleAlert(title: "공연 상세 조회 실패", message: "네트워크 상태를 확인해주세요.")
+            }
+        }
     }
 }
